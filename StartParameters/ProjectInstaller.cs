4fd1eca5e62868cb9bd99bae9043fc6e81b691ed
Jonathan Shelley <andrewster05@yahoo.com>
2023-12-19 10:05:24 -0600

    First

---
+++ StartParameters/ProjectInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace StartParameters
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            serviceInstaller1.Description = "Log Start Parameters";
+            serviceInstaller1.DisplayName = "StartParameters";
+        }
+    }
+}
---
+++ StartParameters/Service.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace StartParameters
+{
+    public partial class Service : ServiceBase
+    {
+        Timer _timer = new Timer(); // name space(using System.Timers;)
+        public Service()
+        {
+            InitializeComponent();
+        }
+        protected override void OnStart(string[] args)
+        {
+            // We have start-up args, lets do something with them!
+            if (args.Length > 0)
+            {
+                WriteToFile("There are arguments here " + DateTime.Now);
+                for (int i = 0; i < args.Length; i++)
+                {
+                    var arg = args[i];
+                    WriteToFile($"{i}. Argument: {arg} " + DateTime.Now);
+                }
+            }
+
+            WriteToFile("Service is started at " + DateTime.Now);
+            _timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            _timer.Interval = 5000; //number in milisecinds
+            _timer.Enabled = true;
+        }
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Service is recall at " + DateTime.Now);
+        }
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
